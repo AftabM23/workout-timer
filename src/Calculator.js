2003ea49d014e5a1623e8537092b6cd656de2753
AftabM23 <aftaba.mohammed23@gmail.com>
2024-09-01 19:00:54 -0400

    added soundeffect for duration change

--- src/Calculator.js
+++ src/Calculator.js
@@ -12,12 +12,17 @@ function Calculator({ workouts, allowSound }) {
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
-
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    [duration, allowSound]
+  );
   useEffect(
     function () {
       setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
